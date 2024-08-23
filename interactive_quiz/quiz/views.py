cee5c8ce6cb98191296d1dd471ff2d9273492a31
TheKingsident <kingsident@gmail.com>
2024-08-23 15:16:57 +0100

    chore: Update home page with days in development

--- interactive_quiz/quiz/views.py
+++ interactive_quiz/quiz/views.py
@@ -1,5 +1,17 @@
 from django.shortcuts import render
+from datetime import datetime
 
 # Create your views here.
 def home(request):
-    return render(request, 'home.html', {})
+    startDevDate = datetime(2024, 8, 22)
+    todaysDate = datetime.now()
+    daysDeveloped = (todaysDate - startDevDate).days
+    days = "days"
+    
+    if daysDeveloped <= 1:
+        days = "day"
+
+    return render(request, 'home.html', {
+        "daysDeveloped": daysDeveloped,
+        "days": days
+    })
