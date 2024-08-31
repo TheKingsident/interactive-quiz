d22100d79cb66c1db06d0b016b0263866d582b54
TheKingsident <kingsident@gmail.com>
2024-08-31 14:45:22 +0100

    chore: Update admin.py, models.py, and templates for quiz app

--- interactive_quiz/quiz/admin.py
+++ interactive_quiz/quiz/admin.py
@@ -2,7 +2,22 @@ from django.contrib import admin
 from .models import Quiz, Option, User, Score
 
 # Register your models here.
-admin.site.register(Quiz)
+# admin.site.register(Quiz)
 admin.site.register(Option)
-admin.site.register(User)
+# admin.site.register(User)
 admin.site.register(Score)
+
+
+@admin.register(Quiz)
+class QuizAdmin(admin.ModelAdmin):
+    list_display = ('question', 'answer', 'topic', 'difficulty')
+    search_fields = ('question', 'answer', 'topic', 'difficulty')
+    list_filter = ('topic', 'difficulty')
+    ordering = ('difficulty',)
+
+
+@admin.register(User)
+class UserAdmin(admin.ModelAdmin):
+    list_display = ('username', 'email')
+    search_fields = ('first_name', 'last_name', 'email')
+    ordering = ('first_name', 'last_name')
--- interactive_quiz/quiz/models.py
+++ interactive_quiz/quiz/models.py
@@ -1,22 +1,6 @@
 from django.db import models
 from django.contrib.auth.models import AbstractUser
 
-# Create your models here.
-class Quiz(models.Model):
-    question = models.CharField(max_length=200)
-    answer = models.CharField(max_length=200)
-    topic = models.CharField(max_length=200)
-    difficulty = models.CharField(max_length=200)
-
-    def __str__(self):
-        return self.question
-    
-class Option(models.Model):
-    option_text = models.CharField(max_length=200)
-    quiz = models.ForeignKey(Quiz, related_name='options', on_delete=models.CASCADE)
-
-    def __str__(self):
-        return self.option_text
 
 class User(AbstractUser):
 
@@ -29,3 +13,20 @@ class Score(models.Model):
 
     def __str__(self):
         return f"{self.user.username}: {self.score}"
+
+class Quiz(models.Model):
+    question = models.CharField(max_length=200)
+    answer = models.CharField(max_length=200)
+    topic = models.CharField(max_length=200, blank=True)
+    difficulty = models.CharField(max_length=200)
+    added_by = models.ForeignKey(User, blank=True, on_delete=models.SET_NULL, null=True)
+
+    def __str__(self):
+        return self.question
+    
+class Option(models.Model):
+    option_text = models.CharField(max_length=200)
+    quiz = models.ForeignKey(Quiz, related_name='options', on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.option_text
